2b185b2775811e1bdcde85ca8d2c8b9dd4ec331d
yanagiraku <n.miu.ovo.november19@gmail.com>
2021-07-30 15:30:45 +0900

    新規登録・ログイン・ログアウト実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :password,        length: { minimum: 6 }, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
+
+  with_options presence: true do 
+    validates :nickname
+    validates :age,           format: { with: /\A[0-9]+\z/ }
+  end
 end
