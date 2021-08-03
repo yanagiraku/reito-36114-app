7f956e7fd07378a618160190c0afc65f05c40041
yanagiraku <n.miu.ovo.november19@gmail.com>
2021-08-03 15:54:42 +0900

    商品詳細リンクの追加

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,8 @@
 class ProductsController < ApplicationController
-# before_action :set_product, only:[:show, :meat, :fish, :cup, :croquette, :rice, :noodle, :side, :pizza, :gratin, :foodstuff, :sweets]
+before_action :set_product, only:[:show, :fish, :cup, :croquette, :rice, :noodle, :side, :pizza, :gratin, :foodstuff, :sweets]
 
   def index
+    @products = Product.all
   end
 
   def new
@@ -24,46 +25,47 @@ class ProductsController < ApplicationController
 
   def meat
     @product1 = Product.where(category_id:2)
+    @product = Product.find_by(category_id:2)
   end
 
   def fish
-    @product2 = Product.where(:category.name="魚系")
+    @product2 = Product.where(category_id:3)
   end
 
   def cup
-    @product3 = Product.where(:category.name= "カップ")
+    @product3 = Product.where(category_id:4)
   end
 
   def croquette
-    @product4 = Product.where(:category.name= "コロッケ")
+    @product4 = Product.where(category_id:5)
   end
 
   def rice
-    @product5 = Product.where(:category.name= "米飯")
+    @product5 = Product.where(category_id:6)
   end
 
   def noodle
-    @product6 = Product.where(:category.name= "麺類")
+    @product6 = Product.where(category_id:7)
   end
 
   def side
-    @product7 = Product.where(:category.name= "お惣菜")
+    @product7 = Product.where(category_id:8)
   end
 
   def pizza
-    @product8 = Product.where(:category.name= "ピザ")
+    @product8 = Product.where(category_id:9)
   end
 
   def gratin
-    @product9 = Product.where(:category.name= "グラタン・ドリア")
+    @product9 = Product.where(category_id:10)
   end
 
   def foodstuff
-    @product10 = Product.where(:category.name="食材")
+    @product10 = Product.where(category_id:11)
   end
 
   def sweets
-    @product11 = Product.where(:category.name= "スイーツ")
+    @product11 = Product.where(category_id:12)
   end
 
 
@@ -73,9 +75,9 @@ class ProductsController < ApplicationController
     params.require(:product).permit(:image, :product_name, :product_num, :description, :ganre_id, :category_id, :cooking_method_id, :cooking_method2_id, :company_id)
   end
 
-  # def set_product
-  #   @product = Product.find(params[:id])
-  # end
+  def set_product
+    @product = Product.find_by(params[:id])
+  end
 
 
 
