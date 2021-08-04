b130e70e814e312affaea22bb58b36f7323bcae3
yanagiraku <n.miu.ovo.november19@gmail.com>
2021-08-04 16:28:51 +0900

    reviews　データベース作成

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,12 +2,14 @@ class Review < ApplicationRecord
   belongs_to :user
   belongs_to :product
 
+  validates :recommend_score_id, numericality: { other_than: 1 }
+  
   with_options presence: true do
     validates :title
-    validates :title
-    validates :title
-    validates :title
-    validates :title
-    validates :title
+    validates :created_day
+    validates :comment
   end
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :recommend_score
 end
